7af2bfc77c6cfb7bc4f1c8729b7fe2c5e1f13604
Romario Rodrigues <romariobdc2011@gmail.com>
2023-12-17 00:43:34 -0300

    Editados alguns arquivos

--- burguer/urls.py
+++ burguer/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
     path('produtos/', produtos),
-]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
